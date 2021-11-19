79a883f840f701f68368d93372b911172af0bb59
Hung Pham <phamnanghung.25@gmail.com>
2021-11-19 22:13:33 +0700

    fix name

--- configs/constants.php
+++ configs/constants.php
@@ -1,7 +1,7 @@
 <?php
     define("HOST", "localhost");
     define("USER", "root");
-    define("PASS", "02100120");
+    define("PASS", "");
     define("DB", "ltw_3");
-    define("DOMAIN","http://localhost:8080/ltw");
+    define("DOMAIN","http://localhost:8080/ltwbt3");
 ?>

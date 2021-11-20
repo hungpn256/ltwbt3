41c84c1b9cc8e90c123cc6f4441e4dc565ee56e8
= <=>
2021-11-20 12:59:17 +0700

    modify pages

--- pages/kinhte.php
+++ pages/kinhte.php
@@ -1,6 +1,7 @@
 <?php
     include_once("../controllers/requireLogin.php")
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,114 +19,17 @@
 
 <body id="page-top">
 
-    <div id="wrapper">
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-                <div class="sidebar-brand-icon">
-                    <img src="https://tuyensinh.ptit.edu.vn/_next/static/images/logo-c5be62e95f69e6f8285d1fd2ee0688ca.png" width="35" height="50">
-                </div>
-                <div class="sidebar-brand-text fs-4 mx-3">PTIT</div>
-            </a>
-            <hr class="sidebar-divider my-0">
-
-            <li class="nav-item active">
-                <a class="nav-link" href="index.php">
-
-                    <span class="fs-5 ">Quản lý Học viện</span></a>
-            </li>
-
-            <hr class="sidebar-divider">
-            <div class="sidebar-heading fs-6">
-                Danh mục
-            </div>
-
-            <li class="nav-item">
-                <a class="nav-link" href="bangtin.php">
-
-                    <span class="fs-6">Bảng tin</span>
-                </a>
-            </li>
-            <!-- -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#NganhHocThuGon" aria-expanded="true" aria-controls="NganhHocThuGon">
-                    <span class="fs-6">Ngành học</span>
-                </a>
-                <div id="NganhHocThuGon" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Chỉnh sửa thông tin</h6>
-                        <a class="collapse-item fs-6" href="congnghe.php">Công nghệ</a>
-                        <a class="collapse-item fs-6" href="kinhte.php">Kinh tế</a>
-
-                    </div>
-                </div>
-            </li>
-            <!-- -->
-            <li class="nav-item">
-                <a class="nav-link" href="student.php">
-
-                    <span class="fs-6">Sinh viên</span></a>
-            </li>
-            <!-- -->
-            <li class="nav-item">
-                <a class="nav-link fs-5" href="point.php">
-
-                    <span class="fs-6">Điểm sinh viên</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link fs-5" href="kyhocmonhoc.php">
-
-                    <span class="fs-6">Kỳ học - Môn học</span></a>
-            </li>
-
-        </ul>
+    <div id="wrapper">       
+        <?php 
+            include_once("sidebar.php");
+        ?>
 
         <div id="content-wrapper" class="d-flex flex-column">
-            <div id="content">
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4  shadow">
-                    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Tìm kiếm..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                    <ul class="navbar-nav ml-auto">
-                        <div class="topbar-divider d-none d-sm-block"></div>
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Tên ADMIN</span>
-                                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Thông tin cá nhân
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Cài đặt
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Lịch sử hoạt động
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                        Đăng xuất
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-                </nav>
-
+            <div id="content">                
+                <div>
+                    <?php include_once("topbar.php");?>
+                </div>
                 <div class="container-fluid">
-
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">BẢNG QUẢN LÝ NGÀNH KINH TẾ</h1>
                     </div>      
@@ -135,28 +39,7 @@
             </div>
         </div>
     </div>
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Modal thông báo đăng xuất-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Bạn muốn dăng xuất?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body"> </div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Hủy</button>
-                    <a class="btn btn-primary" href="login.html">Đăng xuất</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include_once("logout.php")?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="../components/vendor/jquery/jquery.min.js"></script>
